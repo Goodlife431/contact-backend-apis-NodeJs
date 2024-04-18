c7bd6ffe777f6a259819241a86d9b711fa8f7e4d
Goodlife431 <oduwolejohn431@gmail.com>
2024-04-18 10:56:32 +0100

    new

---
+++ config/dbConnection.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require("mongoose");
+
+const connectDb = async () => {
+    try {
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+        console.log("Connected to MongoDB",
+        connect.connection.host, 
+        connect.connection.name);
+    }catch(error){
+        process.exit(1);
+    }
+};
+
+module.exports = connectDb;
