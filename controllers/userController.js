6fe50984c7daa713dc3cd34bcb6504e90e5117db
Goodlife431 <oduwolejohn431@gmail.com>
2024-04-19 12:20:31 +0100

    hasheg password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,17 @@
 const expressAsyncHandler = require("express-async-handler");
 
 const registerUser = expressAsyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are required");
+    }
+
+    const availableUser = await User.findOne( {email});
+    if(availableUser){
+        res.status(400);
+        throw new Error("User already exists");
+    }
     res.json({ message: 'Register the user. '})
 });
 
