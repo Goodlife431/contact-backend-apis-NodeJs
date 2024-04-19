69ae8b06965058e8f1213205248692f6c7e6eec0
Goodlife431 <oduwolejohn431@gmail.com>
2024-04-19 16:58:44 +0100

    access token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const expressAsyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt")
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
 
 const registerUser = expressAsyncHandler(async (req, res) => {
@@ -34,7 +35,31 @@ const registerUser = expressAsyncHandler(async (req, res) => {
 });
 
 const loginUser = expressAsyncHandler(async (req, res) => {
-    res.json({ message: 'login  user. '})
+    const { email, password } = req.body;
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are required");
+    }
+    const user = await User.findOne({email});
+    if (user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                _id: user._id,
+            },
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: 3600,
+        }
+        );
+        res.status(200).json({ accessToken});
+    }else {
+        res.status(401);
+        throw new Error("Email or password is not valid")
+    }
+    res.json({ message: 'login  user. '});
 });
 
 const currentUser = expressAsyncHandler(async (req, res) => {
